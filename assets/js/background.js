3a5b8079bc913b74e726e87c9189310fe1f4adc3
Steven Eysermans <31731363+eynsa@users.noreply.github.com>
2017-09-28 22:09:52 +0200

    committing before refactoring class, once again

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,58 +1,93 @@
-/**
- * returns a view filtered by name
- * @param  {string} name: the filename including extension
- * @return {object} (window)
- */
-function getView(name) {
-  const views = chrome.extension.getViews();
+/** keeping all webRequest helper functions together */
+class WebRequestBlocker { // eslint-disable-line no-unused-vars
+  /**
+  * all web requests made pass through here to determine whether
+  *   they should be blocked.
+  * todo: implement whitelist here.
+  *
+  * @param {object} request - contains details about the intercepted request
+  * @return {BlockingResponse} used to modify network requests
+  */
+  block(request) {
+    let blockingResponse = {cancel: true};
+    return blockingResponse;
+  }
 
-  const result = views.filter(function(view) {
-    let pathname = view.window.location.pathname;
-    let filename = pathname.slice(1, pathname.length);
-    return filename == name;
-  });
-  return result.pop();
-}
+  /**
+   * enables the blocker by attaching the listener
+   * @return {boolean} - is the blocker enabled
+   */
+  enable() {
+    chrome.webRequest.onBeforeRequest.addListener(
+    this.block,
+    {urls: ['http://*/*', 'https://*/*']},
+    ['blocking']);
+    // temporary to show that our blocker works
+    chrome.browserAction.setBadgeText({text: 'ON'});
+    return true;
+  }
 
-/* exported countdownTimer */
-/** triggers when the timer button is clicked */
-function countdownTimer() {
-  const view = getView('popup.html').window;
+  /**
+   * disables the blocker by removing the listener
+   * @return {boolean} - is the blocker enabled
+   */
+  disable() {
+    chrome.webRequest.onBeforeRequest.removeListener(this.block);
+    // temporary to show that our blocker works
+    chrome.browserAction.setBadgeText({text: ''});
+    return false;
+  }
 
-  if (!isBlockerActive()) {
-    enableBlocker();
-    view.updateBlockerStatus();
-  } else {
-    disableBlocker();
-    view.updateBlockerStatus();
+  /** @return {boolean} - is the blocker enabled */
+  isEnabled() {
+    return chrome.webRequest.onBeforeRequest.hasListener(this.block);
+  }
+
+  /**
+   * toggles the blocker on-off
+   * @return {boolean} enabled - is the blocker enabled
+   */
+  toggle() {
+    if (!this.isEnabled()) {
+      this.enable();
+    } else {
+      this.disable();
+    }
+    return this.isEnabled();
   }
 }
 
-/** activates web request blocker */
-function enableBlocker() {
-  chrome.webRequest.onBeforeRequest.addListener(
-  webRequestBlocker, // callback
-  {urls: ['http://*/*', 'https://*/*']}, // filter
-  ['blocking'] // opt_extraInfoSpec ('blocking' means handles synchronously)
-  );
-  chrome.browserAction.setBadgeText({text: 'ON'});
-}
-
-/** removes web request blocker */
-function disableBlocker() {
-  chrome.webRequest.onBeforeRequest.removeListener(webRequestBlocker);
-  chrome.browserAction.setBadgeText({text: 'OFF'});
-}
-
-/** @return {Boolean} */
-function isBlockerActive() {
-  return chrome.webRequest.onBeforeRequest.hasListener(webRequestBlocker);
-}
 
 /**
-* @param {object} request contains details about the intercepted request
-* @return {BlockingResponse} used to modify network requests
-*/
-function webRequestBlocker(request) {
-  return {cancel: true};
+ * listener function that processes messages sent from other pages
+ * @param  {Object} request - JSON object
+ * @param  {Object} sender - the id and url the message originated from
+ * @param  {function} sendResponse - function that sends the JSON response
+ */
+function messageListener(request, sender, sendResponse) {
+  // todo: check if request contains webRequestBlocker name
+  switch (request.WebRequestBlocker) {
+    case 'enable': {
+      sendResponse({isEnabled: blocker.enable()});
+      break;
+    }
+    case 'disable': {
+      sendResponse({isEnabled: blocker.disable()});
+      break;
+    }
+    case 'isEnabled': {
+      sendResponse({isEnabled: blocker.isEnabled()});
+      break;
+    }
+    case 'toggle': {
+      sendResponse({isEnabled: blocker.toggle()});
+      break;
+    }
+    default: {
+      break;
+    }
+  }
 }
+
+const blocker = new WebRequestBlocker();
+chrome.runtime.onMessage.addListener(messageListener);
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,6 +1,27 @@
-const bg = chrome.extension.getBackgroundPage();
+/** Countdown timer logic */
+function countdownTimer() {
+  toggleWebRequestBlocker();
+}
 
-/** @function */
+/** Sends a message to WebRequestBlocker to toggle it on-off */
+function toggleWebRequestBlocker() {
+  let message = {WebRequestBlocker: 'toggle'};
+  chrome.runtime.sendMessage(message, function(response) {
+    let status = response.isEnabled;
+    updateWebRequestBlockerStatus();
+  });
+}
+
+/** Updates page elements to show current blocker status */
+function updateWebRequestBlockerStatus() {
+  let message = {WebRequestBlocker: 'isEnabled'};
+  chrome.runtime.sendMessage(message, function(response) {
+      let status = (response.isEnabled ? 'enabled' : 'disabled');
+      document.querySelector('#blocker').textContent = status;
+  });
+}
+
+/** Recommended method to provide a link to the options page */
 function openOptionsPage() {
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
@@ -9,17 +30,12 @@ function openOptionsPage() {
   }
 }
 
-/** updates page element with the current blocker status */
-function updateBlockerStatus() {
-  const status = document.querySelector('h2');
-  status.textContent = (bg.isBlockerActive() ? 'active' : 'not active');
-}
 
 document.addEventListener('DOMContentLoaded', function() {
-  updateBlockerStatus();
+  updateWebRequestBlockerStatus();
 
   document.querySelector('#timer').addEventListener(
-    'click', bg.countdownTimer);
+    'click', countdownTimer);
 
   document.querySelector('#options').addEventListener(
     'click', openOptionsPage);
