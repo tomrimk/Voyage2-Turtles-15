620dcf04e95788b44808ba2e510253d530078487
Steven Eysermans <31731363+eynsa@users.noreply.github.com>
2017-10-02 13:10:43 +0200

    Validate domain input before entering in whitelist

--- assets/js/options.js
+++ assets/js/options.js
@@ -11,6 +11,35 @@ function log(message) {
 }
 
 /**
+ * Stops the form from submitting when pressing the 'Enter' key.
+ * Validates the domain field and if valid, add it to the whitelist textarea.
+ * @param {Event} event - KeyboardEvent
+ */
+function validateDomainOnEnter(event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+
+    let input = document.querySelector('input[name="domain"');
+    if (isValidDomain(input.value)) {
+      let whitelist = document.querySelector('textarea[name="whitelist"');
+      whitelist.textContent = `${whitelist.textContent}\n${input.value}`;
+    } else {
+      // invalid domain
+    }
+  }
+}
+
+/**
+ * Checks whether the domain is valid
+ * todo: implement
+ * @param {String} domain
+ * @return {boolean} is the input a valid domain
+ */
+function isValidDomain(domain) {
+  return true;
+}
+
+/**
  * Populates form fields with data from storage
  * @param {Object} data - JSON data object
  */
@@ -41,7 +70,14 @@ function submit(event) {
 
 /** document init function */
 function init() {
+  // retrieves form values from storage and puts them in the page
   chrome.storage.local.get(null, fillForm);
+
+  // overrides default behavior when the focus is on domain input field
+  let domainField = document.querySelector('input[name="domain');
+  domainField.addEventListener('keydown', validateDomainOnEnter);
+
+  // what to do when the form is submitted
   document.forms['options'].addEventListener('submit', submit);
 }
 
